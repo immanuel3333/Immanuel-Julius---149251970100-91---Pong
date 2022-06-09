f4ce7d435d11d6c99c68d46a423148f5990d9bd1
immanuel3333 <immanuel.siburian80@gmail.com>
2022-06-09 11:01:08 +0700

    Immanuel-Julius-149251970100-91-Pong

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    //private Vector2 speed;
+    public int speed;
+    public KeyCode upKey;
+    public KeyCode downKey;
+
+    private Rigidbody2D rig;
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        MoveObject(GetInput());
+    }
+
+    private Vector2 GetInput()
+    {
+
+        if (Input.GetKey(upKey))
+        {
+            // Gerakkan ke atas
+            //  speed = new Vector2(0, 1, 0);
+            return Vector2.up * speed;
+        }
+        else if (Input.GetKey(downKey))
+        {
+            // Gerakkan ke bawah
+            return Vector2.down * speed;
+        }
+        return Vector2.zero;
+    }
+
+    private void MoveObject(Vector2 movement)
+    {
+        //Gerakkan object pake input
+        // transform.Translate(movement * Time.deltaTime);
+        rig.velocity = movement;
+
+    }
+}
+
